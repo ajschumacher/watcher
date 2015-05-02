eec92f0e52063a2693f52f3f7fd50d50cc407f32
Aaron Schumacher <ajschumacher@gmail.com>
2015-05-01 21:32:39 -0400

    correct two-week look-back

--- watcher.py
+++ watcher.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 import requests
 
 url = "https://api.github.com/users/{}/events"
+interval = timedelta(days=14)
 
 
 def to_date(date_string):
     return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
+
+
+def float_days(interval):
+    interval_seconds = interval.total_seconds()
+    interval_days = interval_seconds / float(24 * 60 * 60)
+    return interval_days
 
 
 def info_for(username):
@@ -24,9 +31,15 @@ def velocity_of(username):
     dates = [to_date(date_string) for date_string in date_strings]
     earliest = min(dates)
     now = datetime.now()
-    seconds = (now - earliest).total_seconds()
-    float_days = seconds / float(24 * 60 * 60)
-    return len(dates) / float_days
+    then = now - interval
+    if earliest < then:
+        # crop off old actions and accept default period
+        dates = [date for date in dates if then <= date]
+        this_interval = interval
+    else:
+        # period is shorter than the default
+        this_interval = now - earliest
+    return len(dates) / float_days(this_interval)
 
 
 def header():
